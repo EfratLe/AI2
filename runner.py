626aa981f2c8348010d02dafcb4d8ac2a7ffedfb
Daniella Pick <danielapick@gmail.com>
2017-12-24 15:54:10 +0200

    Updated runner to run for sections 0, A-B

--- runner.py
+++ runner.py
@@ -1,10 +1,33 @@
 from subprocess import run
 
-# Section A
-# The parameters are: setup_time, time_per_k_turns, k, verbose, x_player, o_player
+# ===============================================================================
+#                              Section 0
+#                       Trying to play by ourselves
+# ===============================================================================
+# Interactive game against 'simple_player'
+# run("python run_game.py 2 5 5 y interactive simple_player")
+
+# ===============================================================================
+#                              Section A
+# ===============================================================================
+# The parameters for 'run_game.py' are:
+# setup_time, time_per_k_turns, k, verbose, x_player, o_player
+# print("Simple player starts:")
+# for _ in range(3):
+#     run("python run_game.py 2 5 5 n simple_player random_player")
+# print("\nRandom player starts:")
+# for _ in range(3):
+#     run("python run_game.py 2 5 5 n random_player simple_player")
+
+
+# ===============================================================================
+#                              Section B
+# ===============================================================================
+# The parameters for 'run_game.py' are:
+# setup_time, time_per_k_turns, k, verbose, x_player, o_player
 print("Simple player starts:")
 for _ in range(3):
-    run("python run_game.py 2 5 5 n simple_player random_player")
-print("\nRandom player starts:")
+    run("python run_game.py 2 5 5 n simple_player better_player")
+print("\nBetter player starts:")
 for _ in range(3):
-    run("python run_game.py 2 5 5 n random_player simple_player")
+    run("python run_game.py 2 5 5 n better_player simple_player")
